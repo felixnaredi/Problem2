78c549a35eaf08be86af9afef46136e2b28d54cc
felixnaredi <naredi@student.chalmers.se>
2019-08-13 10:10:14 +0200

    Initial Commit

---
+++ Problem2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Problem2
+//
+//  Created by Felix Naredi on 2019-08-13.
+//  Copyright © 2019 Felix Naredi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
